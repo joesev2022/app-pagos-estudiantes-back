38ca1ff946655ce8a96128eb65de438e75d95b79
Jose_Apaza <jose.apazasev@gmail.com>
2025-08-06 15:50:53 -0500

    first commit

---
+++ src/main/java/com/app/estudiantespagos/AppPagosEstudiantesApplication.java
@@ -0,0 +1,13 @@
+package com.app.estudiantespagos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPagosEstudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPagosEstudiantesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/estudiantespagos/AppPagosEstudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.estudiantespagos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPagosEstudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
